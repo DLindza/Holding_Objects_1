f7cd85b415baae3a8e738c0e8a1f64ec6e4e6905
Devon Lindsay <devon@MacBook-Pro.local>
2016-09-29 12:54:26 -0400

    adjust for multiple numbers

--- src/main/java/lindsay/devon/phonebook/PhoneBook.java
+++ src/main/java/lindsay/devon/phonebook/PhoneBook.java
@@ -1,20 +1,18 @@
 package lindsay.devon.phonebook;
 
 import java.util.*;
-//import java.util.stream.Collectors;
 
 /**
  * Created by devon on 9/28/16.
  */
 public class PhoneBook {
 
-    public static Map<String, String> phoneBook = new LinkedHashMap<>();
+    public static Map<String, ArrayList<String>> phoneBook = new LinkedHashMap<>();
 
-    public void addEntry(String name, List<String> phoneNumber) {
+    public void addEntry(String name, ArrayList<String> phoneNumber) {
         System.out.println(name + " has been added to your phonebook.");
-        for (String number : phoneNumber) {
-            phoneBook.put(name, number);
-        }
+        phoneBook.put(name, phoneNumber);
+
     }
 
     public void removeEntry(String name) {
@@ -22,20 +20,27 @@ public class PhoneBook {
         phoneBook.remove(name);
     }
 
+    public void addIndividualPhoneNumber(String name, String phonenumber) {
+        System.out.println(phonenumber + " has been added to " + name + "'s record.");
+        List<String> newNumber = phoneBook.get(name);
+        newNumber.add(phonenumber);
+
+    }
+
     public void removeIndividualPhoneNumber(String name, String phonenumber) {
         System.out.println(phonenumber + " has been removed from your phonebook");
          phoneBook.remove(name,phonenumber);
     }
 
-    public String lookUp(String name) {
-       return phoneBook.get(name);
+    public List<String> lookUp(String name) {
+        return phoneBook.get(name);
     }
 
     public Set<String > listNames() {
         return phoneBook.keySet();
     }
 
-    public Set<Map.Entry<String,String>> getPhoneBook() {
+    public Set<Map.Entry<String,ArrayList<String>>> getPhoneBook() {
       return phoneBook.entrySet();
 
     }
@@ -43,8 +48,10 @@ public class PhoneBook {
     //Thank you - StackOverflow!
     public String reverseLookUp(String phoneNumber) {
         for ( String s : phoneBook.keySet()) {
-            if (phoneBook.get(s).equals(phoneNumber)) {
-                return s;
+            for (String number : phoneBook.get(s)) {
+                if (number.equals(phoneNumber)) {
+                    return s;
+                }
             }
         }  return "Sorry, I couldn't find that selection.";
     }
@@ -52,16 +59,17 @@ public class PhoneBook {
 
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.addEntry("Devon Lindsay", Arrays.asList("508-867-5309", "000-111-2222"));
-        phoneBook.addEntry("Alex Lindsay", Arrays.asList("508-555-5555", "111-222-3333"));
-        phoneBook.addEntry("Erin Lindsay", Arrays.asList("555-555-5555", "333-444-5555"));
+        phoneBook.addEntry("Devon Lindsay", new ArrayList<>(Arrays.asList("508-867-5309", "000-111-2222")));
+        phoneBook.addEntry("Alex Lindsay", new ArrayList<>(Arrays.asList("508-555-5555", "111-222-3333")));
+        phoneBook.addEntry("Erin Lindsay", new ArrayList<>(Arrays.asList("555-555-5555", "333-444-5555")));
 
-        System.out.println(phoneBook.lookUp("Devon"));
-
+        System.out.println("Look up return: " + phoneBook.lookUp("Devon Lindsay"));
         System.out.println(phoneBook.listNames());
         System.out.println(phoneBook.getPhoneBook());
 
        System.out.println(phoneBook.reverseLookUp("508-867-5309"));
+
+
     }
 
 
--- src/test/java/lindsay/devon/phoneBookTest/PhoneBookTest.java
+++ src/test/java/lindsay/devon/phoneBookTest/PhoneBookTest.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
+import java.util.*;
 
 /**
  * Created by devon on 9/28/16.
@@ -18,7 +19,7 @@ public class PhoneBookTest {
    @Test
    public void addEntryTest() {
        PhoneBook phoneBook = new PhoneBook();
-       phoneBook.addEntry("Devon Lindsay", Arrays.asList("555-867-5309", "111-222-3333"));
+       phoneBook.addEntry("Devon Lindsay", new ArrayList<>(Arrays.asList("508-867-5309", "111-222-3333")));
        int expected = 1;
        int actual = PhoneBook.phoneBook.size();
        Assert.assertEquals("Phonebook size should be 1", expected, actual);
@@ -27,10 +28,33 @@ public class PhoneBookTest {
     }
 
     @Test
+    public void removeEntryTest() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addEntry("Devon Lindsay", new ArrayList<>(Arrays.asList("508-867-5309", "111-222-3333")));
+        phoneBook.addEntry("Alex Lindsay", new ArrayList<>(Arrays.asList("555-555-5555", "222-333-4444")));
+        phoneBook.removeEntry("Alex Lindsay");
+        int expected = 1;
+        int actual = PhoneBook.phoneBook.size();
+        Assert.assertEquals("PhoneBook size should be 1", expected,actual);
+
+    }
+
+    @Test
+    public void addIndividualPhoneNumber() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addEntry("Devon Lindsay", new ArrayList<>(Arrays.asList("508-867-5309", "111-222-3333")));
+        phoneBook.addEntry("Alex Lindsay", new ArrayList<>(Arrays.asList("555-555-5555", "222-333-4444")));
+        phoneBook.addIndividualPhoneNumber("Devon Lindsay", "333-444-5555");
+        List<String> expected = new ArrayList<>(Arrays.asList("508-867-5309", "111-222-3333", "333-444-5555"));
+        List<String> actual = phoneBook.lookUp("Devon Lindsay");
+        Assert.assertEquals("Should have 3 phonenumbers", expected, actual);
+    }
+
+    @Test
     public void removeIndividualPhoneNumberTest() {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.addEntry("Devon Lindsay", Arrays.asList( "555-867-5309", "111-222-3333"));
-        phoneBook.addEntry("Alex Lindsay", Arrays.asList("555-555-5555", "222-333-4444"));
+        phoneBook.addEntry("Devon Lindsay", new ArrayList<>(Arrays.asList("508-867-5309", "111-222-3333")));
+        phoneBook.addEntry("Alex Lindsay", new ArrayList<>(Arrays.asList("555-555-5555", "222-333-4444")));
         phoneBook.removeIndividualPhoneNumber("Alex Lindsay", "555-555-5555");
         int expected = 2;
         int actual = PhoneBook.phoneBook.size();
@@ -40,34 +64,34 @@ public class PhoneBookTest {
     @Test
     public void lookUpTest() {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.addEntry("Devon Lindsay", Arrays.asList("508-867-5309", "111-222-3333"));
-        phoneBook.addEntry("Alex Lindsay", Arrays.asList("555-555-5555", "222-333-4444"));
-        String expected = "508-867-5309";
-        String actual = phoneBook.lookUp("Devon Lindsay");
+        phoneBook.addEntry("Devon Lindsay", new ArrayList<>(Arrays.asList("508-867-5309", "111-222-3333")));
+        phoneBook.addEntry("Alex Lindsay", new ArrayList<>(Arrays.asList("555-555-5555", "222-333-4444")));
+        List<String> expected = Arrays.asList("508-867-5309", "111-222-3333");
+        List<String> actual = phoneBook.lookUp("Devon Lindsay");
         Assert.assertEquals("This should be 508-867-5309", expected, actual);
     }
 
     @Test
     public void listNamesTest() {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.addEntry("Devon Lindsay", Arrays.asList("508-867-5309", "111-222-3333"));
-        phoneBook.addEntry("Alex Lindsay", Arrays.asList("555-555-5555", "222-333-4444"));
+        phoneBook.addEntry("Devon Lindsay", new ArrayList<>(Arrays.asList("508-867-5309", "111-222-3333")));
+        phoneBook.addEntry("Alex Lindsay", new ArrayList<>(Arrays.asList("555-555-5555", "222-333-4444")));
         Assert.assertTrue(phoneBook.listNames().contains("Devon Lindsay"));
     }
 
     @Test
     public void getPhoneBookTest() {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.addEntry("Devon Lindsay", Arrays.asList( "508-867-5309", "111-222-3333"));
-        phoneBook.addEntry("Alex Lindsay", Arrays.asList("555-555-5555", "222-333-4444"));
+        phoneBook.addEntry("Devon Lindsay", new ArrayList<>(Arrays.asList("508-867-5309", "111-222-3333")));
+        phoneBook.addEntry("Alex Lindsay", new ArrayList<>(Arrays.asList("555-555-5555", "222-333-4444")));
         Assert.assertNotNull(phoneBook.getPhoneBook());
     }
 
     @Test
     public void reverseLookUpTest() {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.addEntry("Devon Lindsay", Arrays.asList( "508-867-5309", "111-222-3333"));
-        phoneBook.addEntry("Alex Lindsay", Arrays.asList( "555-555-5555", "222-333-4444"));
+        phoneBook.addEntry("Devon Lindsay", new ArrayList<>(Arrays.asList("508-867-5309", "111-222-3333")));
+        phoneBook.addEntry("Alex Lindsay", new ArrayList<>(Arrays.asList("555-555-5555", "222-333-4444")));
         String expected = "Devon Lindsay";
         String actual = phoneBook.reverseLookUp("508-867-5309");
         Assert.assertEquals("This should be Devon Lindsay", expected,actual);
